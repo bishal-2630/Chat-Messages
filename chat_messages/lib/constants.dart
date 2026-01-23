4833e7334910b0ae6942e5128dd710ae3b962552
Bishal <kbishal177@gmail.com>
2026-01-23 14:11:40 +0545

    Updated url

--- chat_messages/lib/constants.dart
+++ chat_messages/lib/constants.dart
@@ -1,8 +1,6 @@
 class ApiConstants {
-  // 1. Find your computer's IP address (e.g., 192.168.1.5)
-  // 2. Replace '10.0.2.2' with that IP address below:
-  static const String host = '192.168.1.76'; 
-  static const String baseUrl = 'http://$host:8000/api';
+
+ static const String baseUrl = 'https://chat-messages-g5zhg726a-bishal-khadkas-projects-e66155cb.vercel.app/api';
   
   // MQTT settings
   static const String mqttBroker = 'broker.emqx.io';
