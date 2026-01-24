211b691752eeb799e9000910fd936cec9e9ff682
Bishal <kbishal177@gmail.com>
2026-01-24 20:20:09 +0545

    Updated api url

--- chat_messages/lib/constants.dart
+++ chat_messages/lib/constants.dart
@@ -1,6 +1,9 @@
 class ApiConstants {
 
- static const String baseUrl = '/api';
+  // For Android Emulator use 'http://10.0.2.2:8000/api'
+  // For Physical Device use 'https://chat-messages-ui.vercel.app/api'
+  static const String baseUrl = 'https://chat-messages-ui.vercel.app/api';
+
   
   // MQTT settings
   static const String mqttBroker = 'broker.emqx.io';
